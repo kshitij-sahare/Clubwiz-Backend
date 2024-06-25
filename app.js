de6195235ce2900c2c0c58dd9eb34e6032974197
kshitijsahare1 <kshitijsahare1@gmail.com>
2024-06-25 13:48:01 +0530

    space

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ app.use(bodyParser.urlencoded({ limit: '50mb', extended: true, parameterLimit: 5
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/events', require('./routes/event'));
 
+
 // Start Server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
